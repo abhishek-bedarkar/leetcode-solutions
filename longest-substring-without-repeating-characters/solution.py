5b86b6ee2069a0c6e920b4f89ba1f2c5b6775b73
abhishek-bedarkar <abhishekbedarkar27@gmail.com>
2023-08-16 19:32:47 -0400

    Solution to longest substring with time complexity o(n^2)

---
+++ longest-substring-without-repeating-characters/solution.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = len(s)
+
+        if l==1 or l==0:
+            return l
+
+        max_length = 1
+        i=0
+        while(i < l-1):
+            length = 1
+            j=i+1
+            
+            if j<=l:
+                while s[j] not in s[i:j]:
+                    length += 1
+                    j += 1
+                    if j>=l:
+                        break
+
+            if length>max_length:
+                max_length = length
+            i += 1
+
+        return max_length
