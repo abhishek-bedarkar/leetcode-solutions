a808444645661686e2a2e440ba7574b5e8125ae4
abhishek_bedarkar <abhishekbedarkar27@gmail.com>
2023-08-25 10:09:50 -0400

    Optimized solution for three sum using two pointers approach

--- 3sum/solution_three_pointer_method.py
+++ 3sum/solution_three_pointer_method.py
@@ -10,7 +10,19 @@ class Solution:
                 break
             target_sum = - sorted_nums[i]
             low = i+1
-            high = len(sorted_nums)
+            high = len(sorted_nums)-1
             while(low != high):
                 if target_sum == sorted_nums[low] + sorted_nums[high]:
-                    pass
+                    temp_sol = [sorted_nums[i],sorted_nums[low],sorted_nums[high]]
+                    if temp_sol not in result:
+                        result.append(temp_sol)
+                    low += 1
+                elif target_sum > sorted_nums[low] + sorted_nums[high]:
+                    low += 1
+                else:
+                    high -= 1
+        return result
+                    
+
+s = Solution()
+print(s.threeSum([-1,0,1,2,-1,-4]))
