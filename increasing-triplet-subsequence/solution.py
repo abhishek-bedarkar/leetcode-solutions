51329620ad2c4c0ae224b2fbfca5bef035f76ce2
abhishek_bedarkar <abhishekbedarkar27@gmail.com>
2023-11-11 12:13:27 -0500

    O(n) approach to solve triple sequence

--- increasing-triplet-subsequence/solution.py
+++ increasing-triplet-subsequence/solution.py
@@ -1,18 +1,19 @@
 class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
+
         if len(nums)<2:
             return False
 
-        #[2,1,5,0,4,6]
-        i = len(nums)-1
-     
+        f_min = float("inf")
+        s_min = float("inf")
 
+        for num in nums:
 
+            if num <= f_min:
+                f_min = num
+            elif num <= s_min:
+                s_min = num
+            else:
+                return True
 
-
-
-
-
-
-
-        
+        return False
