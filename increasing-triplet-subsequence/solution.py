70ebcebd2d2428cd782bdde8ce92809420843150
abhishek_bedarkar <abhishekbedarkar27@gmail.com>
2023-11-10 23:56:10 -0500

    partitial soluton for triple sequence

---
+++ increasing-triplet-subsequence/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(nums)<2:
+            return False
+
+        #[2,1,5,0,4,6]
+        i = len(nums)-1
+     
+
+
+
+
+
+
+
+
+
+        
