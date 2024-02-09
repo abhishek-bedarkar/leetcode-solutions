40d133384f86150e9a36076bfd3ab31021f48400
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-08 21:20:32 -0500

    Time: 3313 ms (29.02%) | Memory: 25.7 MB (16.98%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -3,8 +3,20 @@ class Solution:
     memo = None
     def numSquares(self, n: int) -> int:
         t = math.floor(math.sqrt(n))
-        self.memo = [[-1 for j in range(t+1)] for i in range(n+1)]      
+        self.memo = [[-1 for j in range(t+1)] for i in range(n+1)]
+
+        # Top down approach
+        # for i in range(n+1):
+        #     for j in range(t+1):
+        #         if n ==0 or n==1 or t==1:
+        #             self.memo[n][t] = n
+        #         elif t**2<= n:
+        #             self.memo[n][t] = min(1+self.memo[n-t**2][t], self.memo[n][t-1])
+        #         else:
+        #             self.memo[n][t] = self.memo[n][t-1]
+ 
         return self.ps(n, t)
+        
 
     def ps(self, n:int, t:int)-> int:
         # Base condition   
