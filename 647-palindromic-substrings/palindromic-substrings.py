f1b1c441288495062685649c77436a0d98a1904d
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-10 20:09:45 -0500

    Time: 67 ms (96.09%) | Memory: 16.4 MB (94.09%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,10 +1,42 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        result = 0
-        for i in range(len(s)):
-            for j in range(i,len(s)):
-                sr=s[i:j+1]
-                if s[i:j+1] == sr[::-1]:
-                    result += 1
+        # Brute force 
+        # result = 0
+        # for i in range(len(s)):
+        #     for j in range(i,len(s)):
+        #         sr=s[i:j+1]
+        #         if s[i:j+1] == sr[::-1]:
+        #             result += 1
+        # return result
+
+        # Odd even strategy
+        n, result = len(s), 0
+        
+        def palindromeCount(left: int, right: int) -> int:
+            count = 0
+
+            # Palindrom condition
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+                count += 1
+            return count
+        
+        for i in range(n):
+            # Even cases
+            even = palindromeCount(i, i + 1)
+
+            # Odd cases
+            odd = palindromeCount(i, i)
+            result += even + odd
+            
         return result
+
+
+
+                
+                    
+
+
+
         
