4fc9069707781c2c92da7b7a8c3accdfdb1af894
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-15 13:39:53 -0500

    Time: 561 ms (34.84%) | Memory: 32.3 MB (68.88%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -3,13 +3,15 @@ class Solution:
         sorted_sides = sorted(nums)
         sum_arr = [sum(sorted_sides[:2])]
         a = sum(sorted_sides[:2])
+
+        # Calculate sum array
         for s in sorted_sides[2:]:
             sum_arr.append(a+s)
             a = a+s
-        i=0
-        print(sum_arr)
-        print(sorted_sides)
-        result = -1
+
+        i,result=0,-1
+        
+        # Check polygon condition
         for i in range(len(sum_arr)-1):
             if sum_arr[i] > sorted_sides[i+2]:
                 result = sum_arr[i+1]
