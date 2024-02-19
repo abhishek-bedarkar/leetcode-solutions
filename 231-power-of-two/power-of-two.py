4547db0f06cd6eb806df07ec2e0e280d9498d726
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-18 19:37:24 -0500

    Time: 40 ms (38.25%) | Memory: 16.8 MB (48.58%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,9 +1,17 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        binary = bin(n)[2:]
-        print(binary)
-        if binary.count('1') == 1 and binary[0]=='1':
-            return True
-        else:
-            return False
+        # Decimal to Binary approach
+        # binary = bin(n)[2:]
+        # print(binary)
+        # if binary.count('1') == 1 and binary[0]=='1':
+        #     return True
+        # else:
+        #     return False
+
+        # Modulas approach
+        if n <=0: return False
+        if n==1: return True
+        while n%2==0:
+            n=n/2
+        return n==1
         
