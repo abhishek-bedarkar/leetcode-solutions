2b906f56a2ac97401b89a5528185b0939836df7f
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-19 19:55:51 -0500

    Time: 112 ms (64.33%) | Memory: 17.8 MB (88.15%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -7,13 +7,19 @@ class Solution:
         # return total
 
         # 2. Sorting approach
-        l = len(nums)
-        nums.sort()
+        # l = len(nums)
+        # nums.sort()
 
-        if nums[0] != 0: return 0
-        if nums[-1] != l: return l
-        for i in range(l-1):
-            if nums[i+1] - nums[i] != 1:
-                return nums[i]+1
+        # if nums[0] != 0: return 0
+        # if nums[-1] != l: return l
+        # for i in range(l-1):
+        #     if nums[i+1] - nums[i] != 1:
+        #         return nums[i]+1
+
+        # 3. Series formula 
+        n = len(nums)
+        total = n*(n +1)//2
+        actual_sum = sum(nums)
+        return total - actual_sum
 
         
