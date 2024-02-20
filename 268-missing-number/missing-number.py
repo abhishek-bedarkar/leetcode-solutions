ae23dc17ecd9795d15047318d3cd9a8f81bd83db
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-19 19:50:49 -0500

    Time: 111 ms (68.52%) | Memory: 17.8 MB (88.15%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,19 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        total = sum([ i for i in range(len(nums)+1)])
-        for num in nums:
-            total -= num
-        return total
+        # 1. Without sorting 
+        # total = sum([ i for i in range(len(nums)+1)])
+        # for num in nums:
+        #     total -= num
+        # return total
+
+        # 2. Sorting approach
+        l = len(nums)
+        nums.sort()
+
+        if nums[0] != 0: return 0
+        if nums[-1] != l: return l
+        for i in range(l-1):
+            if nums[i+1] - nums[i] != 1:
+                return nums[i]+1
+
         
