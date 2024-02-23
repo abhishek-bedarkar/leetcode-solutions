758c03648d83f5e80eeb098f7ce511b71106c579
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-02-23 17:36:34 -0500

    Time: 392 ms (70.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select machine_id, round(avg(processing_time),3) as processing_time from 
+(select l.machine_id, l.process_id, r.timestamp - l.timestamp  AS processing_time  
+from Activity l 
+ left join Activity r on l.machine_id = r.machine_id and l.process_id =r.process_id 
+ where  l.activity_type  = 'start' and r.activity_type='end') as subquery group by(machine_id)
