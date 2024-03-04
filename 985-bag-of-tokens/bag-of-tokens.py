309d11e6978491786d5a4d0a9b43e92ff54b2951
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-03-03 21:10:59 -0500

    Time: 51 ms (69.92%) | Memory: 16.7 MB (64.63%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.py
+++ 985-bag-of-tokens/bag-of-tokens.py
@@ -3,31 +3,53 @@ class Solution:
         tokens.sort()
         score = 0
         max_score = 0
-        while tokens:
-            #print("power : ",power, "token :", tokens)
-            # Face up
-            if power > tokens[0]:
-                val = tokens[0]
-                tokens.remove(val)
-                power -= val
-                score += 1 
 
-            # Face down
-            elif score >= 1 and power > 0 and len(tokens) != 1:
-                val = tokens[-1]
-                tokens.remove(val)
-                power += val
+        # brute force approach
+        # while tokens:
+        #     #print("power : ",power, "token :", tokens)
+        #     # Face up
+        #     if power > tokens[0]:
+        #         val = tokens[0]
+        #         tokens.remove(val)
+        #         power -= val
+        #         score += 1 
+
+        #     # Face down
+        #     elif score >= 1 and power > 0 and len(tokens) != 1:
+        #         val = tokens[-1]
+        #         tokens.remove(val)
+        #         power += val
+        #         score -= 1
+        #     # break
+        #     else:
+        #         if power == tokens[0]:
+        #             val = tokens[0]
+        #             tokens.remove(val)
+        #             power -= val
+        #             score += 1
+        #         else:
+        #             break
+        #     max_score = max(score, max_score)
+        # return max_score
+
+        # Two pointers approach
+        left = 0
+        right = len(tokens)-1
+        tokens.sort()
+        while left <= right:
+            #print("power : ",power, "token :", tokens[left:right+1])
+            if power >= tokens[left]:
+                score += 1
+                power -= tokens[left]
+                left += 1
+            elif score >= 1:
                 score -= 1
-            # break
+                power += tokens[right]
+                right -= 1
             else:
-                if power == tokens[0]:
-                    val = tokens[0]
-                    tokens.remove(val)
-                    power -= val
-                    score += 1
-                else:
-                    break
+                break
             max_score = max(score, max_score)
         return max_score
+         
 
         
