4800aa2a74f617f86bd008045b344e716d8cea8d
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-03-09 21:42:08 -0500

    Time: 145 ms (8.45%) | Memory: 37 MB (9.76%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,23 @@
+import numpy as np
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+
+        h_index = np.zeros(len(citations))
+        citations.sort()
+
+        for idx, citation in enumerate(citations):
+            h_index[idx:] += 1
+
+        h_index = h_index[::-1]
+        print(citations)
+        print(h_index)
+        result = 0
+        for i in range(len(citations)):
+            min_citation = min(citations[i], h_index[i])
+            if h_index[i] >= min_citation:
+                result = max(result, min_citation)
+            else:
+                break
+        return int(result)
+
+        
