b541d56d34e1eb8cc0e45aef03d7e86c40e4525c
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-05-03 18:44:12 -0400

    Time: 25 ms (97.66%) | Memory: 16.5 MB (49.28%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,15 +1,16 @@
 class Solution:
 
-    
     def compareVersion(self, version1: str, version2: str) -> int:
-
+        
+        #split numbers
         version1_list = version1.split('.')
         version2_list = version2.split('.')
 
         result = 0
-
+        # iterate for till both list exist
         while len(version1_list) or len(version2_list):
             
+            # get left and right elements
             if version1_list and version2_list:
                 left, right = int(version1_list.pop(0)), int(version2_list.pop(0))
             elif version1_list:
@@ -18,6 +19,8 @@ class Solution:
             else:
                 left = 0
                 right = int(version2_list.pop(0))
+
+            # if large or small return result
             if left > right:
                 return 1
             elif left < right:
