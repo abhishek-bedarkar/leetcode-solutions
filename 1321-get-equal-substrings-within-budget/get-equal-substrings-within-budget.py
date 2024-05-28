02b31d4e95c4a5ee235a4b4ede5fd98b55224fb9
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-05-28 19:29:49 -0400

    Time: 84 ms (5.86%) | Memory: 17.2 MB (70.85%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,31 +1,25 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        diff_arr = []
-
-        for i in range(len(s)):
-            diff_arr.append(abs(ord(s[i]) - ord(t[i])))
         
-        #print(diff_arr)
-
         left, right, diff, max_len = 0, 0, 0, 0
         # [1, 1, 1, 2]
         
-        while left < len(diff_arr) and right< len(diff_arr):
-
-            if diff_arr[right] > maxCost:
+        while left < len(s) and right< len(t):
+            curr_ele_diff = abs(ord(s[right]) - ord(t[right]))
+            if curr_ele_diff > maxCost:
                 left = right + 1
                 right = left
                 diff = 0
             else:
                 # expand window
-                if diff + diff_arr[right] <= maxCost:
-                    diff += diff_arr[right]
+                if diff + curr_ele_diff <= maxCost:
+                    diff += curr_ele_diff
                     right += 1
                     max_len = max(max_len, right - left)
                     #print(f' left {left}, right {right}, max_len {max_len}')
                 # Shrink window
                 else:
-                    diff -= diff_arr[left]
+                    diff -= abs(ord(s[left]) - ord(t[left]))
                     left += 1
         
         return max_len
