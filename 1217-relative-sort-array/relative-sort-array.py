9a7133d67fed31c87997b82694ab815729edf675
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-06-11 11:13:32 -0400

    Time: 39 ms (68.91%) | Memory: 17 MB (10.78%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -3,11 +3,11 @@ class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         result, cnt = [], Counter(arr1)
 
-        for elm in arr2:
-            for i in range(cnt[elm]):
-                result.append(elm)
+        # for elm in arr2:
+        #     for i in range(cnt[elm]):
+        #         result.append(elm)
 
-        # result = [elm for elm in arr2 for i in range(cnt[elm])]
+        result = [elm for elm in arr2 for i in range(cnt[elm])]
         for elm in sorted(set(arr1) - set(arr2)):
             for i in range(cnt[elm]):
                 result.append(elm)
