9e2188d39ef6e069d8222538bb9f8fbf194ee1b6
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-08-08 11:01:56 -0400

    Time: 765 ms (9.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with ranked_activity as (
+    select *,
+         rank() over (partition by player_id order by event_date) as rnk
+    from Activity
+)
+select player_id,
+    ranked_activity.event_date as first_login 
+    from ranked_activity 
+    where ranked_activity.rnk = 1
