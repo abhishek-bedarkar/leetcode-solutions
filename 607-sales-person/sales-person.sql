1c45ea159f8c6203f467352736625d07d1ef2a5c
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-08-08 10:52:06 -0400

    Time: 1452 ms (30.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,9 @@
+
+select name 
+    from SalesPerson 
+    where sales_id not in (
+        select sales_id from Orders 
+            where com_id not in (
+                select com_id from Company where name != 'RED'
+                )
+            );
