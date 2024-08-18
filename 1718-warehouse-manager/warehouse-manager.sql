859f1faab73ceae0447db76b5eb3edd98a8fe0ac
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-08-17 20:45:37 -0400

    Time: 561 ms (61.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select w.name as warehouse_name,
+ sum((p.Width*p.Length*p.Height*w.units)) as volume
+from Warehouse w
+left join Products p
+on w.product_id = p.product_id
+group by w.name
