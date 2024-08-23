e4b7dec338eba365e16679a2ec55f1fbf45c6cca
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-08-23 14:19:10 -0400

    Time: 359 ms (40.43%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
 
 
-with reporting as (
-    select e.name, m.name as manager_name, m.id as m_id
-    from Employee e 
-    left join employee m on e.managerId  = m.id  
-    where e.managerId is not null and m.id is not null
-)
-select manager_name as name 
-from reporting 
-group by m_id having count(1) >=5;
+-- with reporting as (
+--     select e.name, m.name as manager_name, m.id as m_id
+--     from Employee e 
+--     left join employee m on e.managerId  = m.id  
+--     where e.managerId is not null and m.id is not null
+-- )
+-- select manager_name as name 
+-- from reporting 
+-- group by m_id having count(1) >=5;
+
+
+select name from Employee where id in (select managerId from Employee group by managerId having count(managerId)>=5)
