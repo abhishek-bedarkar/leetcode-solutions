574cdd90fa657daa2eb94178640d9a67b4b04c20
Abhishek Manish Bedarkar <abhishekbedarkar27@gmail.com>
2024-11-13 21:56:26 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (29.01%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
-        return []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {} 
+        for i,num in enumerate(nums):
+            hashmap[num] = i
         
+        for i in range(len(nums)):
+            find_elm = target - nums[i]
+            if find_elm in hashmap and hashmap[find_elm] != i:
+                return [i, hashmap[find_elm]]
